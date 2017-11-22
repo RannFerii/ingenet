1aa0a613afdf8b82208badc8fd43289514b321ed
RannFerii <josue_dragon_13@hotmail.com>
2017-11-22 16:34:11 -0600

    desde 0

---
+++ web_application/Controllers/AcountController.cs
@@ -0,0 +1,89 @@
+﻿using ASPNETMVCBlank.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.UI.WebControls;
+using System.Security.Claims;
+using Microsoft.AspNet.Identity;
+using Microsoft.Owin.Security;
+using Models;
+
+namespace ASPNETMVCBlank.Controllers
+{
+    public class AcountController : Controller
+    {
+        // GET: Acount
+        [AllowAnonymous]
+        public ActionResult Login(string returnUrl)
+        {
+            ViewBag.ReturnUrl = returnUrl;
+            return View();
+        }
+        [AllowAnonymous]
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Login(Models.Login data, string returnUrl)
+        {
+            IngenetEntities db = new IngenetEntities();
+            ActionResult Result;
+
+            var user = db.Usuarios.FirstOrDefault(p => p.Correo == data.Correo && p.Cotraseña == data.Cotraseña && p.Estatus == 1);
+            if (user != null)
+            {
+                if (user.Estatus == 1)
+                {
+                    Result = SignInUser(user, data.Rememberme, returnUrl);
+                }
+                else
+                {
+                    Result = View(data);
+                }
+            }
+            else
+            {
+                Result = View(data);
+            }
+            return Result;
+        }
+        [AllowAnonymous]
+        private ActionResult SignInUser(Usuario user, bool rememberMe, string returnUrl)
+        {
+            ActionResult Result;
+            var Claims = new List<Claim>();
+            Claims.Add(new Claim(ClaimTypes.NameIdentifier, user.UsuarioID.ToString()));
+            Claims.Add(new Claim(ClaimTypes.Email, user.Correo));
+            Claims.Add(new Claim(ClaimTypes.Name, user.UserName));
+            Claims.Add(new Claim("fullname", user.Nombres));
+            Claims.Add(new Claim(ClaimTypes.Role, user.Rol));
+            /*
+            if(user.Roles != & user.Roles.Any())
+            {
+                Claims.AddRange(user.Roles.Select(r => new Claim(ClaimTypes.Role, r.Name)));
+            }
+            */
+            var Identity = new ClaimsIdentity(Claims, DefaultAuthenticationTypes.ApplicationCookie);
+
+            IAuthenticationManager AuthenticationManager = HttpContext.GetOwinContext().Authentication;
+            AuthenticationManager.SignIn(new AuthenticationProperties()
+            {
+                IsPersistent = rememberMe
+            }, Identity);
+
+            if (string.IsNullOrEmpty(returnUrl))
+            {
+                returnUrl = Url.Action("Index", "Home");
+            }
+            Result = Redirect(returnUrl);
+            return Result;
+        }
+        [Authorize]
+        public ActionResult LogOff()
+        {
+            IAuthenticationManager AuthenticationManager = HttpContext.GetOwinContext().Authentication;
+            AuthenticationManager.SignOut(DefaultAuthenticationTypes.ApplicationCookie);
+            return RedirectToAction("Login", "Acount");
+        }
+    }
+}
---
+++ web_application/Models/Login.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace ASPNETMVCBlank.Models
+{
+    public class Login
+    {
+        [Required]
+        [Display(Name = "Correo Electrónico")]
+        [DataType(DataType.EmailAddress)]
+        public string Correo { get; set; }
+        [Required]
+        [Display(Name = "Contraseña")]
+        [DataType(DataType.Password)]
+        public string Cotraseña { get; set; }
+
+        [Display(Name = "Recordarme")]
+        public bool Rememberme { get; set; }
+    }
+}
